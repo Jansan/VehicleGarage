57060c56d43c24fccc7ffabb6638df078dbbfcc4
Jansan <jansan-26@hotmail.com>
2020-10-19 15:37:26 +0200

    VehicleType Select

--- VehicleGarage/Models/Vehicle.cs
+++ VehicleGarage/Models/Vehicle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,12 +9,26 @@ namespace VehicleGarage.Models
     public class Vehicle
     {
         public int Id { get; set; }
+
+        [Required]
         public VehicleType VehicleType { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 6, MinimumLength = 6)]
         public string RegNum { get; set; }
+
+        [Required]
         public string Color { get; set; }
+
+        [Required]
         public string Brand { get; set; }
+
+        [Required]
         public string Model { get; set; }
+
+        [Required]
         public int NumWheels { get; set; }
+
         public DateTime ArrivalTime { get; set; }
 
     }
