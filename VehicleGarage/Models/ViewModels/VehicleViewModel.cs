1704d38b54ea4cec42ad7926671204d2a9a7addf
Jansan <jansan-26@hotmail.com>
2020-10-19 16:17:44 +0200

    VehicleViewModel created

---
+++ VehicleGarage/Models/ViewModels/VehicleViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VehicleGarage.Models.ViewModels
+{
+    public class VehicleViewModel
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Vehicle Type")]
+        public VehicleType VehicleType { get; set; }
+
+        [DisplayName("Registration Number")]
+        public string RegNum { get; set; }
+
+        [DisplayName("Arrival Time")]
+        public DateTime ArrivalTime { get; set; }
+
+        
+    }
+}
