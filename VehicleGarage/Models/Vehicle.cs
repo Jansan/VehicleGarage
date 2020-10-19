d20b8f94d682534d9f37b63aa315459ba5876584
Jansan <jansan-26@hotmail.com>
2020-10-19 14:52:27 +0200

    Add project files.

---
+++ VehicleGarage/Models/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VehicleGarage.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public VehicleType VehicleType { get; set; }
+        public string RegNum { get; set; }
+        public string Color { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int NumWheels { get; set; }
+        public DateTime ArrivalTime { get; set; }
+
+    }
+}
