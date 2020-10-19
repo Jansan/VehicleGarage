8b6f61e359e00a881f9bc16d2ee592d798769194
Jansan <jansan-26@hotmail.com>
2020-10-19 15:48:31 +0200

    Park Vehicle Arrivaldate & time Now

--- VehicleGarage/Controllers/VehiclesController.cs
+++ VehicleGarage/Controllers/VehiclesController.cs
@@ -58,6 +58,7 @@ namespace VehicleGarage.Controllers
         {
             if (ModelState.IsValid)
             {
+                vehicle.ArrivalTime = DateTime.Now;
                 _context.Add(vehicle);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
