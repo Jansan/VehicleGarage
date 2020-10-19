380d0d0259f670b10b7d4b202facf454eeb61f52
Jansan <jansan-26@hotmail.com>
2020-10-19 15:15:13 +0200

    VehiclesController created

---
+++ VehicleGarage/Data/VehicleGarageContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using VehicleGarage.Models;
+
+namespace VehicleGarage.Data
+{
+    public class VehicleGarageContext : DbContext
+    {
+        public VehicleGarageContext (DbContextOptions<VehicleGarageContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<VehicleGarage.Models.Vehicle> Vehicle { get; set; }
+    }
+}
