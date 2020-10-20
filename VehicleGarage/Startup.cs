3f35855a1d8b69af0bf705650ccbf861242c3225
Jansan <jansan-26@hotmail.com>
2020-10-20 09:45:18 +0200

    Navbar and footer edited

--- VehicleGarage/Startup.cs
+++ VehicleGarage/Startup.cs
@@ -55,7 +55,7 @@ namespace VehicleGarage
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Vehicles}/{action=Index}/{id?}");
             });
         }
     }
